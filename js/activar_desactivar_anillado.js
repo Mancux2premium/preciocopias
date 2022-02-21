9fe5a105e0e40e55a119921bba573324d624aba9
Mancux2premium <mancux2@gmail.com>
2022-02-21 17:11:47 -0300

    separacion js

---
+++ js/activar_desactivar_anillado.js
@@ -0,0 +1,10 @@
+ //Verifica si la opcion anillado fue tildada o destildada
+ function activarAnillado (){
+    if(document.getElementById("activarAnillado").checked){
+      activaranillado= true;
+    }
+    else {
+      activaranillado = false;
+    }
+ }
+ /*--Final--*/
---
+++ js/botos_ayuda_asterisco_rojo.js
@@ -0,0 +1,7 @@
+/*--muestra una alerta si la ayuda fue consultada--*/
+function boton_ayuda(){
+
+    alert("Numero de veces que desea imprimir el mismo documento");       
+
+}
+ /*--Final--*/
