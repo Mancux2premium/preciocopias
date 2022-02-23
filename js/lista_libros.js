106d2d2c5e54aee4c79b9dd70af8d62e453a301b
Mancux2premium <mancux2@gmail.com>
2022-02-23 11:30:43 -0300

    se agrega lista de libros de la A-Z

--- js/lista_libros.js
+++ js/lista_libros.js
@@ -1,10 +1,191 @@
 /*--Lista de los libros--*/  
-var lista_de_libros = [
+const lista_de_libros = [
 
+        /*--Comienzan con Letra A--*/
         {nombre:'El espejo Africano', paginas:el_espejo_africano_paginas},
         {nombre:'Mi Planta de Naranja lima', paginas:mi_planta_de_naranja_lima_paginas},
         {nombre:'Aladino y la Lampara Maravillosa', paginas:aladino_yla_lampara_maravillosa_paginas},
-                               ]
-          
-/*--Final--*/
+        {nombre:'', paginas:0},                
+        /*--Final A--*/
 
+         /*--Comienzan con Letra B--*/
+         {nombre:'El espejo Africano', paginas:el_espejo_africano_paginas},
+         {nombre:'Mi Planta de Naranja lima', paginas:mi_planta_de_naranja_lima_paginas},
+         {nombre:'Aladino y la Lampara Maravillosa', paginas:aladino_yla_lampara_maravillosa_paginas},
+         {nombre:'', paginas:0},                     
+         /*--Final B--*/
+
+        /*--Comienzan con Letra C--*/
+        {nombre:'El espejo Africano', paginas:el_espejo_africano_paginas},
+        {nombre:'Mi Planta de Naranja lima', paginas:mi_planta_de_naranja_lima_paginas},
+        {nombre:'Aladino y la Lampara Maravillosa', paginas:aladino_yla_lampara_maravillosa_paginas},
+        {nombre:'', paginas:0},                     
+        /*--Final C--*/
+
+        /*--Comienzan con Letra D--*/
+        {nombre:'El espejo Africano', paginas:el_espejo_africano_paginas},
+        {nombre:'Mi Planta de Naranja lima', paginas:mi_planta_de_naranja_lima_paginas},
+        {nombre:'Aladino y la Lampara Maravillosa', paginas:aladino_yla_lampara_maravillosa_paginas},
+        {nombre:'', paginas:0},                   
+        /*--Final D--*/
+
+        /*--Comienzan con Letra E--*/
+        {nombre:'El espejo Africano', paginas:el_espejo_africano_paginas},
+        {nombre:'Mi Planta de Naranja lima', paginas:mi_planta_de_naranja_lima_paginas},
+        {nombre:'Aladino y la Lampara Maravillosa', paginas:aladino_yla_lampara_maravillosa_paginas},
+        {nombre:'', paginas:0},                    
+        /*--Final E--*/
+
+         /*--Comienzan con Letra F--*/
+        {nombre:'El espejo Africano', paginas:el_espejo_africano_paginas},
+        {nombre:'Mi Planta de Naranja lima', paginas:mi_planta_de_naranja_lima_paginas},
+        {nombre:'Aladino y la Lampara Maravillosa', paginas:aladino_yla_lampara_maravillosa_paginas},
+         {nombre:'', paginas:0},                            
+        /*--Final F--*/
+         
+        /*--Comienzan con Letra G--*/
+        {nombre:'El espejo Africano', paginas:el_espejo_africano_paginas},
+        {nombre:'Mi Planta de Naranja lima', paginas:mi_planta_de_naranja_lima_paginas},
+        {nombre:'Aladino y la Lampara Maravillosa', paginas:aladino_yla_lampara_maravillosa_paginas},
+        {nombre:'', paginas:0},                     
+        /*--Final G--*/
+
+        /*--Comienzan con Letra H--*/
+        {nombre:'El espejo Africano', paginas:el_espejo_africano_paginas},
+        {nombre:'Mi Planta de Naranja lima', paginas:mi_planta_de_naranja_lima_paginas},
+        {nombre:'Aladino y la Lampara Maravillosa', paginas:aladino_yla_lampara_maravillosa_paginas},
+        {nombre:'', paginas:0},                         
+        /*--Final H--*/
+
+        /*--Comienzan con Letra I--*/
+        {nombre:'El espejo Africano', paginas:el_espejo_africano_paginas},
+        {nombre:'Mi Planta de Naranja lima', paginas:mi_planta_de_naranja_lima_paginas},
+        {nombre:'Aladino y la Lampara Maravillosa', paginas:aladino_yla_lampara_maravillosa_paginas},
+        {nombre:'', paginas:0},                      
+        /*--Final I--*/
+
+        /*--Comienzan con Letra J--*/
+        {nombre:'El espejo Africano', paginas:el_espejo_africano_paginas},
+        {nombre:'Mi Planta de Naranja lima', paginas:mi_planta_de_naranja_lima_paginas},
+        {nombre:'Aladino y la Lampara Maravillosa', paginas:aladino_yla_lampara_maravillosa_paginas},
+        {nombre:'', paginas:0},           
+        /*--Final J--*/
+
+        /*--Comienzan con Letra K--*/
+        {nombre:'El espejo Africano', paginas:el_espejo_africano_paginas},
+        {nombre:'Mi Planta de Naranja lima', paginas:mi_planta_de_naranja_lima_paginas},
+        {nombre:'Aladino y la Lampara Maravillosa', paginas:aladino_yla_lampara_maravillosa_paginas},
+        {nombre:'', paginas:0},               
+        /*--Final K--*/
+
+         /*--Comienzan con Letra L--*/
+         {nombre:'El espejo Africano', paginas:el_espejo_africano_paginas},
+         {nombre:'Mi Planta de Naranja lima', paginas:mi_planta_de_naranja_lima_paginas},
+         {nombre:'Aladino y la Lampara Maravillosa', paginas:aladino_yla_lampara_maravillosa_paginas},
+         {nombre:'', paginas:0},                  
+         /*--Final L--*/  
+
+        /*--Comienzan con Letra M--*/
+        {nombre:'El espejo Africano', paginas:el_espejo_africano_paginas},
+        {nombre:'Mi Planta de Naranja lima', paginas:mi_planta_de_naranja_lima_paginas},
+        {nombre:'Aladino y la Lampara Maravillosa', paginas:aladino_yla_lampara_maravillosa_paginas},
+        {nombre:'', paginas:0},                     
+        /*--Final M--*/
+
+        /*--Comienzan con Letra N--*/
+        {nombre:'El espejo Africano', paginas:el_espejo_africano_paginas},
+        {nombre:'Mi Planta de Naranja lima', paginas:mi_planta_de_naranja_lima_paginas},
+        {nombre:'Aladino y la Lampara Maravillosa', paginas:aladino_yla_lampara_maravillosa_paginas},
+        {nombre:'', paginas:0},                  
+        /*--Final N--*/
+
+        /*--Comienzan con Letra Ñ--*/
+        {nombre:'El espejo Africano', paginas:el_espejo_africano_paginas},
+        {nombre:'Mi Planta de Naranja lima', paginas:mi_planta_de_naranja_lima_paginas},
+        {nombre:'Aladino y la Lampara Maravillosa', paginas:aladino_yla_lampara_maravillosa_paginas},
+        {nombre:'', paginas:0},                    
+        /*--Final Ñ--*/
+
+        /*--Comienzan con Letra O--*/
+        {nombre:'El espejo Africano', paginas:el_espejo_africano_paginas},
+        {nombre:'Mi Planta de Naranja lima', paginas:mi_planta_de_naranja_lima_paginas},
+        {nombre:'Aladino y la Lampara Maravillosa', paginas:aladino_yla_lampara_maravillosa_paginas},
+        {nombre:'', paginas:0},                    
+        /*--Final O--*/
+
+        /*--Comienzan con Letra P--*/
+        {nombre:'El espejo Africano', paginas:el_espejo_africano_paginas},
+        {nombre:'Mi Planta de Naranja lima', paginas:mi_planta_de_naranja_lima_paginas},
+        {nombre:'Aladino y la Lampara Maravillosa', paginas:aladino_yla_lampara_maravillosa_paginas},
+        {nombre:'', paginas:0},                       
+        /*--Final P--*/
+
+        /*--Comienzan con Letra Q--*/
+        {nombre:'El espejo Africano', paginas:el_espejo_africano_paginas},
+        {nombre:'Mi Planta de Naranja lima', paginas:mi_planta_de_naranja_lima_paginas},
+        {nombre:'Aladino y la Lampara Maravillosa', paginas:aladino_yla_lampara_maravillosa_paginas},
+        {nombre:'', paginas:0},                      
+        /*--Final Q--*/
+
+        /*--Comienzan con Letra R--*/
+        {nombre:'El espejo Africano', paginas:el_espejo_africano_paginas},
+        {nombre:'Mi Planta de Naranja lima', paginas:mi_planta_de_naranja_lima_paginas},
+        {nombre:'Aladino y la Lampara Maravillosa', paginas:aladino_yla_lampara_maravillosa_paginas},
+        {nombre:'', paginas:0},                     
+        /*--Final R--*/
+
+        /*--Comienzan con Letra S--*/
+        {nombre:'El espejo Africano', paginas:el_espejo_africano_paginas},
+        {nombre:'Mi Planta de Naranja lima', paginas:mi_planta_de_naranja_lima_paginas},
+        {nombre:'Aladino y la Lampara Maravillosa', paginas:aladino_yla_lampara_maravillosa_paginas},
+        {nombre:'', paginas:0},                    
+        /*--Final S--*/
+
+        /*--Comienzan con Letra T--*/
+        {nombre:'El espejo Africano', paginas:el_espejo_africano_paginas},
+        {nombre:'Mi Planta de Naranja lima', paginas:mi_planta_de_naranja_lima_paginas},
+        {nombre:'Aladino y la Lampara Maravillosa', paginas:aladino_yla_lampara_maravillosa_paginas},
+        {nombre:'', paginas:0},                       
+        /*--Final T--*/
+
+        /*--Comienzan con Letra U--*/
+        {nombre:'El espejo Africano', paginas:el_espejo_africano_paginas},
+        {nombre:'Mi Planta de Naranja lima', paginas:mi_planta_de_naranja_lima_paginas},
+        {nombre:'Aladino y la Lampara Maravillosa', paginas:aladino_yla_lampara_maravillosa_paginas},
+        {nombre:'', paginas:0},                     
+        /*--Final U--*/
+
+        /*--Comienzan con Letra V--*/
+        {nombre:'El espejo Africano', paginas:el_espejo_africano_paginas},
+        {nombre:'Mi Planta de Naranja lima', paginas:mi_planta_de_naranja_lima_paginas},
+        {nombre:'Aladino y la Lampara Maravillosa', paginas:aladino_yla_lampara_maravillosa_paginas},
+        {nombre:'', paginas:0},                    
+        /*--Final V--*/
+
+        /*--Comienzan con Letra W--*/
+        {nombre:'El espejo Africano', paginas:el_espejo_africano_paginas},
+        {nombre:'Mi Planta de Naranja lima', paginas:mi_planta_de_naranja_lima_paginas},
+        {nombre:'Aladino y la Lampara Maravillosa', paginas:aladino_yla_lampara_maravillosa_paginas},
+        {nombre:'', paginas:0},                       
+        /*--Final W--*/
+
+        /*--Comienzan con Letra X--*/
+        {nombre:'El espejo Africano', paginas:el_espejo_africano_paginas},
+        {nombre:'Mi Planta de Naranja lima', paginas:mi_planta_de_naranja_lima_paginas},
+        {nombre:'Aladino y la Lampara Maravillosa', paginas:aladino_yla_lampara_maravillosa_paginas},
+        {nombre:'', paginas:0},                     
+        /*--Final X--*/
+
+        /*--Comienzan con Letra Y--*/
+        {nombre:'El espejo Africano', paginas:el_espejo_africano_paginas},
+        {nombre:'Mi Planta de Naranja lima', paginas:mi_planta_de_naranja_lima_paginas},
+        {nombre:'Aladino y la Lampara Maravillosa', paginas:aladino_yla_lampara_maravillosa_paginas},
+        {nombre:'', paginas:0},                
+        /*--Final Y--*/
+
+        /*--Comienzan con Letra Z--*/
+        {nombre:'El espejo Africano', paginas:el_espejo_africano_paginas},
+        {nombre:'Mi Planta de Naranja lima', paginas:mi_planta_de_naranja_lima_paginas},
+        {nombre:'Aladino y la Lampara Maravillosa', paginas:aladino_yla_lampara_maravillosa_paginas},
+        {nombre:'', paginas:0},                       
+        /*--Final Z--*/]
