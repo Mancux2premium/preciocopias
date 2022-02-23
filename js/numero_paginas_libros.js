c05eacdfd9005759a8f10edab40971af0ef570df
Mancux2premium <mancux2@gmail.com>
2022-02-23 12:15:47 -0300

    se agregan const paginas de nuevos libros

--- js/numero_paginas_libros.js
+++ js/numero_paginas_libros.js
@@ -1,3 +1,87 @@
-var el_espejo_africano_paginas=81;
-var mi_planta_de_naranja_lima_paginas=150;
-var aladino_yla_lampara_maravillosa_paginas=27;
+/*--paginas que contienen cada libro en formato pdf*--/
+
+/*--Comienzan con Letra A--*/
+const aladino_y_la_lampara_maravillosa_paginas=27;
+/*--Final A--*/
+
+/*--Comienzan con Letra B--*/
+/*--Final B--*/
+
+/*--Comienzan con Letra C--*/
+const caro_dice_paginas=242;
+/*--Final C--*/
+
+/*--Comienzan con Letra D--*/
+const de_como_romeo_se_tranzo_a_julieta_paginas=127;
+/*--Final D--*/
+
+/*--Comienzan con Letra E--*/
+const el_espejo_africano_paginas=81;
+/*--Final E--*/
+
+/*--Comienzan con Letra F--*/
+/*--Final F--*/
+
+/*--Comienzan con Letra G--*/
+/*--Final G--*/
+
+/*--Comienzan con Letra H--*/
+/*--Final H--*/
+
+/*--Comienzan con Letra I--*/
+/*--Final I--*/
+
+/*--Comienzan con Letra J--*/
+/*--Final J--*/
+
+/*--Comienzan con Letra K--*/
+/*--Final K--*/
+
+/*--Comienzan con Letra L--*/
+/*--Final L--*/
+
+/*--Comienzan con Letra M--*/
+const mi_planta_de_naranja_lima_paginas=150;
+/*--Final M--*/
+
+/*--Comienzan con Letra N--*/
+/*--Final N--*/
+
+/*--Comienzan con Letra Ñ--*/
+/*--Final Ñ--*/
+
+/*--Comienzan con Letra O--*/
+/*--Final O--*/
+
+/*--Comienzan con Letra P--*/
+/*--Final P--*/
+
+/*--Comienzan con Letra Q--*/
+/*--Final Q--*/
+
+/*--Comienzan con Letra R--*/
+/*--Final R--*/
+
+/*--Comienzan con Letra S--*/
+/*--Final S--*/
+
+/*--Comienzan con Letra T--*/
+/*--Final T--*/
+
+/*--Comienzan con Letra W--*/
+/*--Final W--*/
+
+/*--Comienzan con Letra V--*/
+/*--Final V--*/
+
+/*--Comienzan con Letra W--*/
+/*--Final W--*/
+
+/*--Comienzan con Letra X--*/
+/*--Final X--*/
+
+/*--Comienzan con Letra Y--*/
+/*--Final Y--*/
+
+/*--Comienzan con Letra Z--*/
+/*--Final Z--*/
