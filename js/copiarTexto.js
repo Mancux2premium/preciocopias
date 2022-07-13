a0815ddfc71d899dfb7f3e2604104729dc230add
Mancux2 <mancux2@gmail.com>
2022-07-13 09:14:45 -0300

    listasPdf

--- js/copiarTexto.js
+++ js/copiarTexto.js
@@ -1,6 +1,6 @@
 function copiarTexto(){
   
-    const nombrePdfCopiado = document.getElementById('nombrePdfCopiado');
+    //const nombrePdfCopiado = document.getElementById('nombrePdfCopiado');
 
     console.log('hac copiado el texto');
     
--- js/lector_hojas_pdf.js
+++ js/lector_hojas_pdf.js
@@ -63,19 +63,23 @@ fileInput.addEventListener('change', (e)=>{
 
 const fileInput = document.getElementById('file')
 let SubirPDF = document.getElementById('subirPDF')
-const nombreDocumento = document.getElementById('nombre')
+//const nombreDocumento = document.getElementById('nombre')
 const precioPdf = document.getElementById('precioPdf')
 const text = document.getElementById('text')
+const ListaPdfCargados = document.getElementById('ListaPdfCargados')
+
 
 
 fileInput.addEventListener('change',(e) =>{
 
 const files  = e.target.files;
-const fragment = document.createDocumentFragment()
+
 
 for(const file of files){
+  const fragment = document.createDocumentFragment()
   const fileReader = new FileReader();
-    console.log(file)
+  const listaPdfCargados = document.createElement('div')
+    /* console.log(file) */
   fileReader.readAsBinaryString(file);
 
 fileReader.addEventListener('load', (e) =>{
@@ -87,8 +91,13 @@ fileReader.addEventListener('load', (e) =>{
     a = document.getElementById("a").value=parseInt(cantidad_hojas_del_pdf_subido_por_el_usuario.length);
     calcular_precio_fotocopias();
 
-    console.log(file.name + " Pagina: " );
-    nombreDocumento.textContent +=`-${file.name} te sale: $${result.value} + son: ${cantidad_hojas_del_pdf_subido_por_el_usuario.length} `
+  /*   console.log(file.name + " Pagina: " );
+    nombreDocumento.innerText +=`-${file.name} te sale: $${result.value} doble faz  son:${cantidad_hojas_del_pdf_subido_por_el_usuario.length} Hojas\n`
+ */
+    listaPdfCargados.textContent += `-${file.name} te sale: $${result.value} son:${cantidad_hojas_del_pdf_subido_por_el_usuario.length} hojas `;
+  
 })
+  fragment.appendChild(listaPdfCargados)
+  ListaPdfCargados.appendChild(fragment)
 }
 });
